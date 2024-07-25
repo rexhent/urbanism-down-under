064c255a07403674a3d469836c5652477767e81b
Dominic <67210734+rexhent@users.noreply.github.com>
2024-07-25 10:26:55 +1000

    fix color mode

--- components/ColorMode.vue
+++ components/ColorMode.vue
@@ -1,21 +1,33 @@
 
 <template>
 <div flex="~">
-    <button flex="~" p="1" border="none" rounded="~" bg="none" @click="switchColor">{{ theme }}</button>
+    <button flex p="1" border="none" rounded bg="none" @click="switchColor">{{ theme }}</button>
 </div>
 
 </template>
 <script setup lang="ts">
 const colorMode = useColorMode()
-const theme: Ref<string> = ref(colorMode.preference == "dark" ? "☀️" : "🌙")
+const theme: Ref<string> = ref("")
+
+onMounted(() => {
+    if (colorMode.preference == "dark") {
+        theme.value = "☀️"
+    } else if (colorMode.preference == "light") {
+        theme.value = "🌙";
+    } else {
+        colorMode.preference = "light"
+        theme.value = "🌙"
+    }
+})
+
 
 const switchColor = () => {
     if (colorMode.preference == "dark") {
         colorMode.preference = "light"
-        theme.value = "☀️";
+        theme.value = "🌙";
     } else {
         colorMode.preference = "dark"
-        theme.value = "🌙";
+        theme.value = "☀️";
     }
 }
 </script>
